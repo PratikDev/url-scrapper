3f6339f92f63cdadac734f11affd82e6a632ba56
pratikdev <pratikdevofficial1@gmail.com>
2024-10-24 18:13:12 +0600

    taking user input for url

--- main.go
+++ main.go
@@ -14,12 +14,22 @@ func main() {
 	results := []string{}
 	var seen = []string{}
 
-	const URL = "https://scrape-me.dreamsofcode.io"
+	var URL string
+
+	fmt.Println("Enter URL to scrape:")
+	fmt.Scanln(&URL)
 
 	wg.Add(1)
 	go utils.GetValidURLs(wg, mut, &results, &seen, URL)
 	wg.Wait()
 
+	fmt.Println("") // line break
 	fmt.Println("Total URLs found:", len(results))
 	fmt.Println("Total seen URLs:", len(seen))
+
+	fmt.Println("") // line break
+	fmt.Println("Valid URLs:")
+	for _, url := range results {
+		fmt.Println(url)
+	}
 }
